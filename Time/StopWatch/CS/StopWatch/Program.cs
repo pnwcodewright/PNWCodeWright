fbd729aed3c9a4a77f0e8da3794309a3fe90bf5c
Jim Wright <james@jimwright.org>
2019-09-28 00:50:11 -0700

    Initial Check In

---
+++ Time/StopWatch/CS/StopWatch/Program.cs
@@ -0,0 +1,57 @@
+﻿//----------------------------------------------------------------------------
+//
+//  $Workfile: StopWatch.cs$
+//
+//  $Revision: X$
+//
+//  Project:    PNW Code Wright
+//
+//                            Copyright (c) 2019
+//                               James A Wright
+//                            All Rights Reserved
+//
+//  Modification History:
+//  $Log:
+//  $
+//
+//----------------------------------------------------------------------------
+using System;
+using System.Threading;
+
+namespace StopWatch
+{
+    //----------------------------------------------------------------------------
+    //  Class Declarations
+    //----------------------------------------------------------------------------
+    //
+    // Class Name: Program
+    // 
+    // Purpose:
+    //      This class tests the StopWatch Class
+    //
+    //----------------------------------------------------------------------------
+    class Program
+    {
+        //--------------------------------------------------------------------
+        // Purpose:
+        //     Test the StopWatch
+        //
+        // Notes:
+        //     
+        //--------------------------------------------------------------------
+        static void Main(string[] args)
+        {
+            StopWatch fiveSeconds = new StopWatch(5 * StopWatch.ONE_SECOND);
+            Console.WriteLine("Waiting five seconds");
+            fiveSeconds.Reset();
+            while (false == fiveSeconds.IsExpired()) 
+            {
+                // Do Nothing
+            }
+            Console.WriteLine("Done");
+            Console.WriteLine("Now:" + StopWatch.Now().ToString());
+            Thread.Sleep(1234);
+            Console.WriteLine("Now:" + fiveSeconds.GetTimeLeft().ToString());
+        }
+    }
+}
---
+++ Time/StopWatch/CS/StopWatch/StopWatch.cs
@@ -0,0 +1,139 @@
+﻿//----------------------------------------------------------------------------
+//
+//  $Workfile: StopWatch.cs$
+//
+//  $Revision: X$
+//
+//  Project:    PNW Code Wright
+//
+//                            Copyright (c) 2019
+//                               James A Wright
+//                            All Rights Reserved
+//
+//  Modification History:
+//  $Log:
+//  $
+//
+//----------------------------------------------------------------------------
+using System;
+
+//----------------------------------------------------------------------------
+//  The name StopWatch is used due to microcontrollers have clocks and
+//  timers, so this name does not collide with other deffinitions
+//----------------------------------------------------------------------------
+
+
+namespace StopWatch
+{
+    //----------------------------------------------------------------------------
+    //  Class Declarations
+    //----------------------------------------------------------------------------
+    //
+    // Class Name: StopWatch
+    // 
+    // Purpose:
+    //      Sets a time to wait for things.
+    //
+    //----------------------------------------------------------------------------
+    class StopWatch
+    {
+        //----------------------------------------------------------------------------
+        //  Public Class Consts
+        //----------------------------------------------------------------------------
+        public const long ONE_SECOND = 1000;
+
+        //----------------------------------------------------------------------------
+        //  Class Attributes 
+        //----------------------------------------------------------------------------
+        long mLastTime = 0;
+        long mWaitTime = ONE_SECOND;
+
+        //--------------------------------------------------------------------
+        // Purpose:
+        //     Empty Constructor
+        //
+        // Notes:
+        //     Keeps the attributes the same as initialized
+        //--------------------------------------------------------------------
+        public StopWatch()
+        {
+        }
+
+        //--------------------------------------------------------------------
+        // Purpose:
+        //     Constructor
+        //
+        // Notes:
+        //     Initializes the Wait Time
+        //--------------------------------------------------------------------
+        public StopWatch(long waitTime)
+        {
+            mWaitTime = waitTime;
+        }
+
+        //--------------------------------------------------------------------
+        // Purpose:
+        //     It returns the current milliseconds from 2018
+        //
+        // Notes:
+        //     The date time can be adjusted to the time frame needed
+        //--------------------------------------------------------------------
+        public static long Now()
+        {
+            TimeSpan now = DateTime.Now - new DateTime(2018,1,1);
+          return (long)now.TotalMilliseconds;
+        }
+
+        //--------------------------------------------------------------------
+        // Purpose:
+        //     Resets the amount to wait 
+        //
+        // Notes:
+        //     None
+        //--------------------------------------------------------------------
+        public void SetTime(long waitTime)
+        {
+            mWaitTime = waitTime;
+        }
+
+        //--------------------------------------------------------------------
+        // Purpose:
+        //     Return true if the stop watch is expired 
+        //
+        // Notes:
+        //     None
+        //--------------------------------------------------------------------
+        public bool IsExpired()
+        {
+            if ((Now() - mLastTime) > mWaitTime)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        //--------------------------------------------------------------------
+        // Purpose:
+        //     Sets the timer to the current time 
+        //
+        // Notes:
+        //     None
+        //--------------------------------------------------------------------
+        public void Reset()
+        {
+            mLastTime = Now();
+        }
+
+        //--------------------------------------------------------------------
+        // Purpose:
+        //     Return the time left to wait
+        //
+        // Notes:
+        //     None
+        //--------------------------------------------------------------------
+        public long GetTimeLeft()
+        {
+            return mWaitTime - (Now() - mLastTime);
+        }
+    }
+}
